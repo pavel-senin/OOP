24978014b5301f5aef3210cbe3b15afd7a499766
pavel-senin <120603369+pavel-senin@users.noreply.github.com>
2023-03-16 00:39:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        GenTree Root = new GenTree("A");
+        Root.addTo("B", "F");
+        Root.addTo("C", "M");
+        Root.addTo("D", "S");
+        Root.addTo("E", "S");
+        Root.addTo("F", "C");
+        Root.addTo("G", "C");
+        System.out.println("Family tree output");
+        Root.ShowRelatives();
+    }
+
+
+
+
+}
